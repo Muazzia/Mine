e6635548cdb4c2bff804c0a76fe54cb1fa8f084c
Muaz <you@example.com>
2023-06-21 20:45:08 +0500

    changing the key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "58c21096114e400c9beebe505fea902a",
   },
 });
