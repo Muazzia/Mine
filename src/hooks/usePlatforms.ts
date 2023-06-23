ed319c45683848d01738a87a3d54c9366042a762
Muaz <you@example.com>
2023-06-24 00:42:21 +0500

    changing in platfomr

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 interface Platform {
   id: number;
@@ -6,6 +9,14 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () => {
+  return useQuery<FetchResponse<Platform>, Error>({
+    queryKey: ["platform"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms")
+        .then((res) => res.data),
+  });
+};
 
 export default usePlatforms;
